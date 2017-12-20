df324fa242835808559155f18016f5d69cb7b11f
Ali Rana <muhammadali@muhammads-macbook-pro.local>
2017-12-20 20:07:19 +0500

    Initial Commit

---
+++ NestedArrayAlgorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NestedArrayAlgorithms
+//
+//  Created by Muhammad Ali on 20/12/2017.
+//  Copyright © 2017 Muhammad Ali. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
